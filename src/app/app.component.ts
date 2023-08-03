0cb2cb12175f972044284f8f17e216e57a3a2e3e
mohameddbenhamou <mohamedbenhamou.info@gmail.com>
2023-08-03 13:59:48 +0100

    adding delete,add,done functionalityes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  tasks:Task[]=[new Task("task1"),new Task("task2"),new Task("task3"),new Task("task4")];
+  add(newTask:string){
+    this.tasks.push(new Task(newTask));
+  }
+  remove(existingTask:Task){
+    var userConfermed=confirm(`Are you sure you want to delete this task?\n ${existingTask}`);
+    if(userConfermed){
+      this.tasks=this.tasks.filter(task=>task!==existingTask);
+    }
+  }
+  markAsDone(task:Task){
+    task.isDone=true;
+    }
+
+  }
+class Task{
+  constructor(public title:string){}
+  isDone:boolean=false;
 }
